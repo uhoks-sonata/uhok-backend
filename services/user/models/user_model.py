f21ac9e2fa58983781f390621d030e26dbaac57f
khangte <minhyuk00321@gmail.com>
2025-07-31 14:43:04 +0900

    chore: 테이블명 & 컬럼명 대분자로 í정의

--- services/user/models/user_model.py
+++ services/user/models/user_model.py
@@ -7,21 +7,21 @@ from services.user.database import Base
 from datetime import datetime
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "USERS"
 
-    user_id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(255), unique=True, nullable=False)
-    password_hash = Column(String(255), nullable=False)
-    username = Column(String(100))
-    created_at = Column(DateTime, default=datetime.utcnow)
+    user_id = Column("USER_ID", Integer, primary_key=True, autoincrement=True)
+    email = Column("EMAIL", String(255), unique=True, nullable=False)
+    password_hash = Column("PASSWORD_HASH", String(255), nullable=False)
+    username = Column("USERNAME", String(100))
+    created_at = Column("CREATE_AT", DateTime, default=datetime.utcnow)
 
     settings = relationship("UserSetting", back_populates="user", uselist=False)
 
 class UserSetting(Base):
-    __tablename__ = "user_settings"
+    __tablename__ = "USER_SETTINGS"
 
-    setting_id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False, unique=True)
-    receive_alerts = Column(Boolean, default=True)
+    setting_id = Column("SETTING_ID", Integer, primary_key=True, autoincrement=True)
+    user_id = Column("USER_ID", Integer, ForeignKey("users.user_id"), nullable=False, unique=True)
+    receive_notification = Column("REVEICE_NOTIFICATION", default=True)
 
     user = relationship("User", back_populates="settings")
