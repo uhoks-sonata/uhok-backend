394b32312c3b2e53994e3f4a9f1d5f80376bc38a
MinHyeok Kang <115693185+khangte@users.noreply.github.com>
2025-08-10 14:45:09 +0900

    chore: 라우터 순서 수정

--- gateway/main.py
+++ gateway/main.py
@@ -10,14 +10,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from common.config import get_settings
-from common.logger import logger
 from services.user.routers.user_router import router as user_router
 from services.log.routers.log_router import router as log_router
 from services.kok.routers.kok_router import router as kok_router
+# from services.home_shopping.routers.home_shopping_router import router as home_shopping_router
 from services.order.routers.order_router import router as order_router
 from services.recipe.routers.recipe_router import router as recipe_router
-
-# from services.home_shopping.routers.home_shopping_router import router as home_shopping_router
 
 # TODO: 다른 서비스(router) import 추가 (recommend 등)
 
@@ -41,22 +39,21 @@ app.add_middleware(
 app.include_router(user_router)
 app.include_router(log_router)
 app.include_router(kok_router)
+# app.include_router(home_shopping_router)
 app.include_router(order_router)
 app.include_router(recipe_router)
 
-# app.include_router(home_shopping_router)
 
 
 # TODO: 다른 서비스 라우터도 아래와 같이 추가
 # from services.recommend.routers.recommend_router import router as recommend_router
-# app.include_router(recommend_router, prefix="/api/recommend")
+# app.include_router(recommend_router)
 
 # 공통 예외 처리 (필요시)
 # from common.errors import *
 # @app.exception_handler(...)
 # async def custom_exception_handler(...):
 #     ...
-
 
 # if __name__ == "__main__":
 #     uvicorn.run("gateway.main:app", host="0.0.0.0", port=8000, reload=True)
