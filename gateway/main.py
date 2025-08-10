aa220afab549d3842f6f747a5c8f677c203cbee2
MinHyeok Kang <115693185+khangte@users.noreply.github.com>
2025-08-10 11:51:17 +0900

    sylte: router 호출 순서 변경

--- gateway/main.py
+++ gateway/main.py
@@ -12,10 +12,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from common.config import get_settings
 from common.logger import logger
 from services.user.routers.user_router import router as user_router
-from services.recipe.routers.recipe_router import router as recipe_router
+from services.log.routers.log_router import router as log_router
 from services.kok.routers.kok_router import router as kok_router
 from services.order.routers.order_router import router as order_router
-from services.log.routers.log_router import router as log_router
+from services.recipe.routers.recipe_router import router as recipe_router
+
 # from services.home_shopping.routers.home_shopping_router import router as home_shopping_router
 
 # TODO: 다른 서비스(router) import 추가 (recommend 등)
@@ -38,10 +39,11 @@ app.add_middleware(
 
 # 라우터 등록 (각 서비스별 router를 include)
 app.include_router(user_router)
-app.include_router(recipe_router)
+app.include_router(log_router)
 app.include_router(kok_router)
 app.include_router(order_router)
-app.include_router(log_router)
+app.include_router(recipe_router)
+
 # app.include_router(home_shopping_router)
 
 
