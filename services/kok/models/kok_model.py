0e2168dcac83db29ec0e2632e9141ddb6a7aca37
seosebin <seominbin@gmail.com>
2025-08-19 16:09:51 +0900

    콕 및 홈쇼핑 model 수정

--- services/kok/models/kok_model.py
+++ services/kok/models/kok_model.py
@@ -250,3 +250,14 @@ class KokNotification(MariaBase):
     # user = relationship('User', backref='notifications')  # User 모델이 별도 서비스에 있을 경우
     # kok_order = relationship('KokOrder', backref='notifications')  # KokOrder 모델이 별도 서비스에 있을 경우
     # status = relationship('StatusMaster', backref='notifications')  # StatusMaster 모델이 별도 서비스에 있을 경우
+
+class KokClassify(MariaBase):
+    """
+    KOK_CLASSIFY 테이블의 ORM 모델
+    콕 제품의 식재료 분류 정보를 저장
+    """
+    __tablename__ = "KOK_CLASSIFY"
+
+    product_id = Column("PRODUCT_ID", Integer, primary_key=True, autoincrement=False, comment='콕 제품 코드')
+    product_name = Column("PRODUCT_NAME", Text, nullable=False, comment='제품명')
+    cls_ing = Column("CLS_ING", Integer, nullable=True, comment='식재료 분류')
